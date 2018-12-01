370152ef32aea61ca2b5fc3b13b40347fecf4c6b
HugeName <suoandong@gmail.com>
2018-12-01 13:46:29 +0800

    Update btpanel5.9.sh

--- btpanel5.9.sh
+++ btpanel5.9.sh
@@ -15,12 +15,14 @@ startTime=`date +%s`
 wget -O update.sh http://download.bt.cn/install/update_pro.sh && bash update.sh pro
 cd /www/server/panel/class
 mv common.py common.py.bak
+yum install unzip -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/HugeName/BT5.9Crack/master/common.zip
 unzip common.zip
 rm common.zip
 rm /root/btpanel.sh
 cd /www/server/panel/data
 touch userInfo.json
+echo > userInfo.json
 /etc/init.d/bt restart
 
 echo "====================================="
