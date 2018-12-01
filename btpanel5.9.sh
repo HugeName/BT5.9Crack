58cdb55a8f12c0cbff2bb61b1e3972a19613bf7f
HugeName <suoandong@gmail.com>
2018-12-01 13:36:36 +0800

    Update and rename btpanel.sh to btpanel5.9.sh

--- btpanel5.9.sh
+++ btpanel5.9.sh
@@ -3,7 +3,7 @@ while [ "$go" != 'y' ] && [ "$go" != 'n' ]
 do
 clear
 read -p "
-宝塔专业版破解补丁 By:HugeName
+宝塔5.9开心版一键工具 By:HugeName
 
 是否安装补丁？(y/n):" go;
 done
@@ -15,7 +15,7 @@ startTime=`date +%s`
 wget -O update.sh http://download.bt.cn/install/update_pro.sh && bash update.sh pro
 cd /www/server/panel/class
 mv common.py common.py.bak
-wget -N --no-check-certificate https://okoke.cn/bt/common.zip
+wget -N --no-check-certificate https://raw.githubusercontent.com/HugeName/BT5.9Crack/master/common.zip
 unzip common.zip
 rm common.zip
 rm /root/btpanel.sh
@@ -24,4 +24,4 @@ touch userInfo.json
 /etc/init.d/bt restart
 
 echo "====================================="
-echo "恭喜您,宝塔专业版破解补丁已成功安装!";
+echo "恭喜您,宝塔5.9开心版已成功安装!";
