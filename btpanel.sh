6308d2c2989ffc0956f4502a5730f7f247a88b96
HugeName <suoandong@gmail.com>
2018-12-01 13:33:08 +0800

    Add files via upload

---
+++ btpanel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+while [ "$go" != 'y' ] && [ "$go" != 'n' ]
+do
+clear
+read -p "
+宝塔专业版破解补丁 By:HugeName
+
+是否安装补丁？(y/n):" go;
+done
+
+if [ "$go" = 'n' ];then
+	exit;
+fi
+startTime=`date +%s`
+wget -O update.sh http://download.bt.cn/install/update_pro.sh && bash update.sh pro
+cd /www/server/panel/class
+mv common.py common.py.bak
+wget -N --no-check-certificate https://okoke.cn/bt/common.zip
+unzip common.zip
+rm common.zip
+rm /root/btpanel.sh
+cd /www/server/panel/data
+touch userInfo.json
+/etc/init.d/bt restart
+
+echo "====================================="
+echo "恭喜您,宝塔专业版破解补丁已成功安装!";
